0c1225d180c92ee1c029739604bd072d26c0e047
Maykell Sánchez Romero <kellsaro@gmail.com>
2024-09-19 21:47:11 +0000

    Add system test on user login

--- test/system/users_logins_test.rb
+++ test/system/users_logins_test.rb
@@ -1,9 +1,23 @@
 require "application_system_test_case"
 
 class UsersLoginsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_logins_url
-  #
-  #   assert_selector "h1", text: "UsersLogin"
-  # end
+  test "existing user can login" do
+    visit root_path
+    click_on I18n.t("shared.navbar.login")
+    fill_in User.human_attribute_name(:email), with: "jerry@example.com"
+    fill_in User.human_attribute_name(:password), with: "wrong"
+    click_button I18n.t("sessions.new.submit")
+
+    assert_selector ".notification.is-danger",
+      text: I18n.t("sessions.create.incorrect_details")
+
+    fill_in User.human_attribute_name(:email), with: "jerry@example.com"
+    fill_in User.human_attribute_name(:password), with: "password"
+    click_button I18n.t("sessions.new.submit")
+
+    assert_current_path root_path
+    assert_selector ".notification",
+      text: I18n.t("sessions.create.success")
+    assert_selector ".navbar-dropdown", visible: false
+    end
 end
